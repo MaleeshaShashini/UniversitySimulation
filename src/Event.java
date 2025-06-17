0bb5a82fab6cbc67f6b78ca6d2b7768ad851ffa7
MaleeshaShashini <maleeshashashini70@gmail.com>
2025-06-17 19:13:39 +0530

    feat: Add getter methods for Event fields

--- src/Event.java
+++ src/Event.java
@@ -1,8 +1,41 @@
 
 public class Event {
-
-	public Event() {
-		// TODO Auto-generated constructor stub
+	private String eventId;
+	private String type;
+	private String location;
+    private int startTime;
+    private int endTime;
+    private String priorityLevel;
+    private int priorityValue;
+    private int capacityRequired;
+	
+    // Constructor to create an Event object
+	public Event(String eventId, String type, String location, int startTime, int endTime, String priorityLevel,int priorityValue, int capacityRequired) {
+		 this.eventId = eventId;
+	     this.type = type;
+	     this.type = type;
+	     this.location = location;
+	     this.startTime = startTime;
+	     this.endTime = endTime;
+	     this.priorityLevel = priorityLevel;
+	     this.capacityRequired = capacityRequired;
+	}
+	
+	
+	private void setPriorityValue(String level) {
+		switch (level.toLowerCase()) {
+        case "mandatory":
+            this.priorityValue = 3;
+            break;
+        case "high-profile":
+            this.priorityValue = 2;
+            break;
+        case "optional":
+            this.priorityValue = 1;
+            break;
+        default:
+            this.priorityValue = 0;
+            }
 	}
 
 }
